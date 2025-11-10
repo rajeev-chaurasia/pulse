9c1aa34f4f110e2a8ad2b6dfd964dd3ca048e719
Rajeev Chaurasia <rajeevchaurasia.dev@gmail.com>
2025-11-10 10:00:00 -0800

    PULSE-01: Initialize project scaffold and monorepo structure

---
+++ pulse-ingestion/src/main/java/com/pulse/ingestion/PulseIngestionApplication.java
@@ -0,0 +1,12 @@
+package com.pulse.ingestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PulseIngestionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PulseIngestionApplication.class, args);
+    }
+}
---
+++ pulse-processor/src/main/java/com/pulse/processor/MatchingJob.java
@@ -0,0 +1,7 @@
+package com.pulse.processor;
+
+public class MatchingJob {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Flink Job Starting...");
+    }
+}
